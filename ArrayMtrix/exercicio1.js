424f4777c4bc33a71585daf163e9f58643aa48e1
Felixluh <luizafelix.senai@gmail.com>
2025-02-25 11:26:03 -0300

    25.02.25

---
+++ ArrayMtrix/exercicio1.js
@@ -0,0 +1,16 @@
+//Criar
+let arrayTexto = ["Maça", "Banana", "Laranja", "Uva"] 
+
+//Exibir
+console.log(arrayTexto[2]);
+
+//Adicionar Melancia
+arrayTexto.unshift("Melancia");
+console.log(arrayTexto);
+
+//Remover
+arrayTexto.shift();
+console.log(arrayTexto);
+
+//Comprimento
+console.log(arrayTexto.length);
